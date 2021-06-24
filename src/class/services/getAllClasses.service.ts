6cfc693ce3faa5160ac8f60524640ed4c98f6681
Ederson S <edersonrodrigo31@gmail.com>
2021-06-24 12:07:42 -0300

    feat(backend): get all classes

--- src/class/services/getAllClasses.service.ts
+++ src/class/services/getAllClasses.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { Class } from '../class.interface';
+import { ClassDocument, Clazz } from '../class.schema';
 
 @Injectable()
 export class GetAllClassService {
+  constructor(
+    @InjectModel(Clazz.name) private classModel: Model<ClassDocument>,
+  ) {}
   async execute(): Promise<Class[]> {
-    return [];
+    const response = await this.classModel.find();
+    return response;
   }
 }
