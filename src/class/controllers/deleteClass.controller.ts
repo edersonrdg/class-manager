f049833eff49baae8478903753443ab96a4cddcb
Ederson S <edersonrodrigo31@gmail.com>
2021-06-24 12:48:02 -0300

    feat(backend): ensure delete class controller calls service

--- src/class/controllers/deleteClass.controller.ts
+++ src/class/controllers/deleteClass.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Delete, Param } from '@nestjs/common';
+import { DeleteClassService } from '../services';
 
 @Controller('class')
 export class DeleteClassController {
+  constructor(private readonly deleteClassService: DeleteClassService) {}
+
   @Delete(':id')
   async handle(@Param('id') id: string): Promise<void> {
-    return;
+    await this.deleteClassService.execute(id);
   }
 }
--- src/class/services/deleteClass.service.ts
+++ src/class/services/deleteClass.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class DeleteClassService {}
+export class DeleteClassService {
+  async execute(id: string): Promise<void> {
+    return;
+  }
+}
