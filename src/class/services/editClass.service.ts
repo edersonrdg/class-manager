1aa648645179a9def4592186e58a29667e4181d1
Ederson S <edersonrodrigo31@gmail.com>
2021-06-24 14:59:29 -0300

    feat(backend): edit class

--- src/class/services/editClass.service.ts
+++ src/class/services/editClass.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ClassDocument, Clazz } from '../class.schema';
 import { EditClassDTO } from '../dtos/editClassDTO';
 
 @Injectable()
 export class EditClassService {
+  constructor(
+    @InjectModel(Clazz.name) private classModel: Model<ClassDocument>,
+  ) {}
   async execute(data: EditClassDTO, id: string): Promise<void> {
-    return;
+    await this.classModel
+      .findOneAndUpdate()
+      .where({ _id: id })
+      .update(data)
+      .catch(() => {
+        throw new BadRequestException({ message: 'Invalid class data' });
+      });
   }
 }
