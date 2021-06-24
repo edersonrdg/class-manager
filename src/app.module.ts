96ea245b49617e59be8f5f38a776ed12e5775076
Ederson S <edersonrodrigo31@gmail.com>
2021-06-24 15:37:06 -0300

    feat(backend): add create student controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ClassModule } from './class/class.module';
+import { ClassModule } from './clazz/class.module';
 import { StudentModule } from './student/student.module';
 
 @Module({
--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CreateStudentController } from './controllers';
 
-@Module({})
+@Module({
+  controllers: [CreateStudentController],
+})
 export class StudentModule {}
