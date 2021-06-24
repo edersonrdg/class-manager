92faa348aee25e9f2ad4428698d6385747685e9b
Ederson S <edersonrodrigo31@gmail.com>
2021-06-24 17:08:45 -0300

    feat(backend): get all students

--- src/student/services/getAllStudents.service.ts
+++ src/student/services/getAllStudents.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { Student } from '../student.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Student, StudentDocument } from '../student.schema';
 
 @Injectable()
 export class GetAllStudentsService {
+  constructor(
+    @InjectModel(Student.name) private studentModel: Model<StudentDocument>,
+  ) {}
+
   async execute(): Promise<Student[]> {
-    return [];
+    const response = await this.studentModel.find();
+    return response;
   }
 }
