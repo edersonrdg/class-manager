de7e58b9bff1f1319ab99079aa074caef2b15e26
Ederson S <edersonrodrigo31@gmail.com>
2021-06-24 12:54:59 -0300

    feat(backend): delete class

--- src/class/services/deleteClass.service.ts
+++ src/class/services/deleteClass.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ClassDocument, Clazz } from '../class.schema';
 
 @Injectable()
 export class DeleteClassService {
+  constructor(
+    @InjectModel(Clazz.name) private classModel: Model<ClassDocument>,
+  ) {}
+
   async execute(id: string): Promise<void> {
-    return;
+    await this.classModel
+      .findOneAndDelete()
+      .where({ _id: id })
+      .catch(() => {
+        throw new BadRequestException({ message: 'Invalid class id' });
+      });
   }
 }
