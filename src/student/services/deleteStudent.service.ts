68f0e6e45a805e0242703aafc97186c59f014a33
Ederson S <edersonrodrigo31@gmail.com>
2021-06-24 17:27:54 -0300

    feat(backend): delete student

--- src/student/services/deleteStudent.service.ts
+++ src/student/services/deleteStudent.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Student, StudentDocument } from '../student.schema';
 
 @Injectable()
 export class DeleteStudentService {
+  constructor(
+    @InjectModel(Student.name) private studentModel: Model<StudentDocument>,
+  ) {}
+
   async execute(id: string): Promise<void> {
-    return;
+    await this.studentModel
+      .findOneAndRemove()
+      .where({ _id: id })
+      .catch(() => {
+        throw new BadRequestException({ message: 'Invalid Student id' });
+      });
   }
 }
