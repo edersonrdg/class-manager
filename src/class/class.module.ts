e0d619039e0058826c816e1eef5d12c0b6f97ffb
Ederson S <edersonrodrigo31@gmail.com>
2021-06-24 11:58:38 -0300

    feat(backend): create get all classes controller

--- src/class/class.module.ts
+++ src/class/class.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClassSchema, Clazz } from './class.schema';
-import { CreateClassController } from './controllers';
+import { CreateClassController, GetAllClassController } from './controllers';
 import { CreateClassService } from './services';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Clazz.name, schema: ClassSchema }]),
   ],
-  controllers: [CreateClassController],
+  controllers: [CreateClassController, GetAllClassController],
   providers: [CreateClassService],
 })
 export class ClassModule {}
--- src/class/controllers/index.ts
+++ src/class/controllers/index.ts
@@ -1,1 +1,2 @@
 export * from './createClass.controller';
+export * from './getAllClasses.controller';
