cd359c24cc1517a0aeb65361da95d04a92074c3b
Ederson S <edersonrodrigo31@gmail.com>
2021-06-24 15:31:21 -0300

    feat(backend): add student module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClassModule } from './class/class.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nestmongo'),
     ClassModule,
+    StudentModule,
   ],
   controllers: [AppController],
   providers: [],
