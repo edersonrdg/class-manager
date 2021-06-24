f779bc19fb77e838ae5668a53ed4187d6f5bed0e
Ederson S <edersonrodrigo31@gmail.com>
2021-06-24 11:40:28 -0300

    feat(backend): ensure create user controller calls service

--- src/class/controllers/createClass.controller.ts
+++ src/class/controllers/createClass.controller.ts
@@ -1,10 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { Class } from '../class.interface';
 import { CreateClassDTO } from '../dtos/createClassDTO';
+import { CreateClassService } from '../services';
 
 @Controller('class')
 export class CreateClassController {
+  constructor(private readonly createClassService: CreateClassService) {}
+
   @Post('/')
-  handle(@Body() userData: CreateClassDTO): boolean {
-    return true;
+  async handle(@Body() userData: CreateClassDTO): Promise<Class> {
+    const response = await this.createClassService.execute(userData);
+    return response;
   }
 }
--- src/class/services/createClass.service.ts
+++ src/class/services/createClass.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Class } from '../class.interface';
+import { CreateClassDTO } from '../dtos/createClassDTO';
 
 @Injectable()
-export class CreateClassService {}
+export class CreateClassService {
+  async execute(data: CreateClassDTO): Promise<Class> {
+    return await { id: '123', name: data.name, year: data.year };
+  }
+}
