c7e792fd382fa1800f394eaa4a2a84b3d5c3f7fd
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-06-25 13:05:53 -0300

    feat(frontend): create input component

---
+++ frontend/src/components/Input/index.tsx
@@ -0,0 +1,26 @@
+import { useField } from '@unform/core';
+import React, { InputHTMLAttributes, useRef, useEffect } from 'react';
+
+// import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    name: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, ...rest }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const { fieldName, defaultValue, registerField } = useField(name);
+
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef.current,
+            path: 'value',
+        });
+    }, [fieldName, registerField]);
+
+    return <input defaultValue={defaultValue} ref={inputRef} {...rest} />;
+};
+
+export default Input;
