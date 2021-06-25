ce2b332035cd626d668e70d3fb22c6106af2a7af
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-06-25 09:32:54 -0300

    refactor(frontend): app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 
-function App() {
+const App: React.FC = () => {
     return (
         <BrowserRouter>
             <GlobalStyle />
             <Routes />
         </BrowserRouter>
     );
-}
+};
 
 export default App;
