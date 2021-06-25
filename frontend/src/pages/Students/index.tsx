299f8898e366a7ca44fabc6ba49bb2dfa827fc4b
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-06-25 10:57:44 -0300

    feat(frontend): student page

--- frontend/src/pages/Students/index.tsx
+++ frontend/src/pages/Students/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import Header from '../../components/Header';
+
+import {
+    Container,
+    Content,
+    StudentCard,
+    StudentCardContent,
+    DeleteStudentCard,
+} from './styles';
 
 const Students: React.FC = () => {
-    return <h1>Alunos</h1>;
+    return (
+        <>
+            <Header>Alunos Turma A</Header>
+            <Container>
+                <Content>
+                    <StudentCard>
+                        <StudentCardContent>
+                            <h4>123123</h4>
+                            <h4>Joao da silva</h4>
+                            <h4>Joao@gmail.com</h4>
+                        </StudentCardContent>
+                        <DeleteStudentCard>
+                            <h4>Del</h4>
+                        </DeleteStudentCard>
+                    </StudentCard>
+                </Content>
+                <form>
+                    <input type="text" placeholder="Código do aluno" required />
+                    <input type="text" placeholder="Primeiro nome" required />
+                    <input type="text" placeholder="Ultimo nome" required />
+                    <input type="email" placeholder="Email" required />
+
+                    <button type="button">Adicionar Aluno</button>
+                </form>
+            </Container>
+        </>
+    );
 };
 
 export default Students;
---
+++ frontend/src/pages/Students/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+export const Content = styled.div``;
+export const StudentCard = styled.div``;
+export const StudentCardContent = styled.div``;
+export const DeleteStudentCard = styled.div``;
