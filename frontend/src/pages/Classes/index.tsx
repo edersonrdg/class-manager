98a1a97e2734bbe5df97f16c47354ff8f8a803ab
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-06-25 09:54:20 -0300

    feat(frontend): class page

--- frontend/src/pages/Classes/index.tsx
+++ frontend/src/pages/Classes/index.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import Header from '../../components/Header';
 
+import {
+    Container,
+    ClassCard,
+    CardContentContainer,
+    StudentContainer,
+    DeleteClassContainer,
+} from './styles';
+
 const Classes: React.FC = () => {
-    return <Header buttonText="Adicionar turma">Gestão de turmas</Header>;
+    return (
+        <>
+            <Header buttonText="Adicionar turma">Gestão de turmas</Header>
+            <Container>
+                <ClassCard>
+                    <CardContentContainer>
+                        <h4>a</h4>
+                        <h4>a</h4>
+                        <h4>a</h4>
+                    </CardContentContainer>
+                    <StudentContainer>
+                        <h4>Alunos</h4>
+                    </StudentContainer>
+                    <DeleteClassContainer>
+                        <h4>DEl</h4>
+                    </DeleteClassContainer>
+                </ClassCard>
+            </Container>
+        </>
+    );
 };
 
 export default Classes;
---
+++ frontend/src/pages/Classes/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const ClassCard = styled.div``;
+
+export const CardContentContainer = styled.div``;
+
+export const StudentContainer = styled.div``;
+
+export const DeleteClassContainer = styled.div``;
