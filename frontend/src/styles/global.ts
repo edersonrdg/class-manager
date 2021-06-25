7f800e6318bf2436be2ba3a95069795104378bec
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-06-25 00:01:07 -0300

    feat(frontend): global styles

---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+    }
+    body {
+        background: #EAEFF3;
+    }
+    body, input, button {
+        font-family: 'Roboto', sans-serif;
+        color: #4A4B64;
+        font-size: 16px;
+    }
+    button {
+        cursor: pointer;
+    }
+`;
