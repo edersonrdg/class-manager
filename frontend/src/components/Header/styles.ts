ab293de09fb9c2f85e424bb200d53043363ca24d
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-06-25 09:40:02 -0300

    styles(frontend): Header component

--- frontend/src/components/Header/styles.ts
+++ frontend/src/components/Header/styles.ts
@@ -1,3 +1,22 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 36px 135px;
+    background-color: #ffffff;
+    border-bottom: #dbd8d8 1px solid;
+
+    h1 {
+        font-size: 24px;
+    }
+
+    button {
+        background-color: #4c4fef;
+        color: #ffffff;
+        font-size: 14px;
+        border: none;
+        border-radius: 6px;
+        padding: 10px 30px;
+    }
+`;
