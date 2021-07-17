e805fab46f57beb2e6c3d3922cdea96c3bf9e5e9
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-07-17 14:18:02 -0300

    test: ensure delete class controller to be defined

---
+++ backend/src/clazz/controllers/deleteClass.controller.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { DeleteClassService } from '../services';
+import { DeleteClassController } from './deleteClass.controller';
+
+describe('DeleteClassController', () => {
+  let controller: DeleteClassController;
+  let service: DeleteClassService;
+
+  class DeleteClassServiceMock {
+    async execute(id: string): Promise<void> {
+      return new Promise((resolve) => resolve());
+    }
+  }
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [DeleteClassController],
+      providers: [DeleteClassService],
+    })
+      .overrideProvider(DeleteClassService)
+      .useClass(DeleteClassServiceMock)
+      .compile();
+
+    controller = module.get<DeleteClassController>(DeleteClassController);
+    service = module.get<DeleteClassService>(DeleteClassService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
