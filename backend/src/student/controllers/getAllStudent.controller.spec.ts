e4768bcd3432685b77aa30097daed101c60222f7
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-07-18 12:51:44 -0300

    test: ensure get all student controller to be defined

---
+++ backend/src/student/controllers/getAllStudent.controller.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { GetAllStudentsService } from '../services';
+import { GetAllStudentsController } from './getAllStudents.controller';
+
+describe('GetAllStudentsController', () => {
+  let controller: GetAllStudentsController;
+  let service: GetAllStudentsService;
+
+  class GetAllStudentsServiceMock {
+    async execute(): Promise<void> {
+      return new Promise((resolve) => resolve());
+    }
+  }
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [GetAllStudentsController],
+      providers: [GetAllStudentsService],
+    })
+      .overrideProvider(GetAllStudentsService)
+      .useClass(GetAllStudentsServiceMock)
+      .compile();
+
+    controller = module.get<GetAllStudentsController>(GetAllStudentsController);
+    service = module.get<GetAllStudentsService>(GetAllStudentsService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
